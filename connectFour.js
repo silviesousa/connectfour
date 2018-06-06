2103401f64317dd9417cbdfafa9c3bb9ca2005f7
Silvia Sousa <info@silviasousa.com>
2018-06-06 16:04:48 +0200

    commiting Connect Four to a public repository

---
+++ connectFour.js
@@ -0,0 +1,133 @@
+(function (){
+    var curPlayer = 'player1';
+    console.log($('.column').length);
+
+    function switchPlayers() {
+
+        $('.chip').removeClass(curPlayer);
+
+        if (curPlayer == 'player1') {
+            curPlayer = 'player2';
+        } else {
+            curPlayer = 'player1';
+        }
+        $('.chip').addClass(curPlayer);
+    }
+
+    function checkForVictory(slots) {
+        var str='';
+        for (var i = 0; i <slots.length; i++) {
+            if (slots.eq(i).hasClass(curPlayer)) {
+                str += 'y';
+            } else {
+                str += 'n';
+            }
+        }
+        return str.indexOf('yyyy') > -1;
+
+    }
+
+    function checkSlotDiag(column, row) {
+
+
+        var upRight = [
+            column.prev().prev().prev().children().eq(row+3),
+            column.prev().prev().children().eq(row+2),
+            column.prev().children().eq(row+1),
+            column.children().eq(row),
+            column.next().children().eq(row-1),
+            column.next().next().children().eq(row-2),
+            column.next().next().next().children().eq(row-3)
+        ];
+
+        var downRight = [
+            column.prev().prev().prev().children().eq(row-3),
+            column.prev().prev().children().eq(row-2),
+            column.prev().children().eq(row-1),
+            column.children().eq(row),
+            column.next().children().eq(row+1),
+            column.next().next().children().eq(row+2),
+            column.next().next().next().children().eq(row+3)
+        ];
+
+        for (var i = 0, str = ''; i < 7; i++){
+            if (upRight[i].hasClass(curPlayer)) {
+                str += 'y';
+            } else {
+                str += 'n';
+            }
+        }
+
+        if (str.indexOf('yyyy') > -1) {
+            return true;
+
+        } else {
+            for (i = 0, str = ''; i < 7; i++){
+                if (downRight[i].hasClass(curPlayer)) {
+                    str += 'y';
+                } else {
+                    str += 'n';
+                }
+            }
+            return str.indexOf('yyyy') > -1;
+
+        }
+    }
+
+    function victoryMessage() {
+        var win = "Congrats! You win!";
+        var animation;
+        setTimeout(function() {
+            if (curPlayer == 'player1') {
+                alert(win);
+                animation.easeIn();
+                location.reload();
+            } else {
+                alert (win);
+                location.reload();
+                animation.easeIn();
+            }
+        }, 600);
+    }
+
+
+    $('body').on('mousemove', function (e){
+        $('.chip').css('top', + (e.clientY - 55));
+        $('.chip').css('left', + (e.clientX - 55));
+    });
+
+
+    $('.column').on('click', function (e) {
+        console.log('click!');
+        var slotsInColumn = $(e.currentTarget).find('.slot');
+        console.log(slotsInColumn.length);
+        for (var i = 5; i >= 0; i--) {
+            if (!slotsInColumn.eq(i).hasClass('player1') && !slotsInColumn.eq(i).hasClass('player2')){
+                slotsInColumn.eq(i).addClass(curPlayer);
+                break;
+            }
+        }
+
+        if (checkForVictory(slotsInColumn)) {
+            console.log('COLUMN YAY');
+            victoryMessage();
+        } else {
+
+            var slotsInRow = $('.row' + i);
+            if (checkForVictory(slotsInRow)) {
+                console.log('ROW YAY');
+                victoryMessage();
+
+            } else {
+                if (checkSlotDiag($(e.currentTarget), i)) {
+                    console.log('DIAGONAL YAY');
+                    victoryMessage();
+                }
+
+            }
+        }
+
+        switchPlayers();
+
+    });
+})();
